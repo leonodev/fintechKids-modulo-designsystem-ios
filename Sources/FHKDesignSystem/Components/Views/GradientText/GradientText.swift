b752220bd9887f959439223801c7c68291934cfc
Fredy Leon <fredy.leon@ua.la.com>
2025-12-20 09:58:34 +0100

    change init view

--- Sources/FHKDesignSystem/Components/Views/GradientText/GradientText.swift
+++ Sources/FHKDesignSystem/Components/Views/GradientText/GradientText.swift
@@ -10,20 +10,36 @@ import SwiftUI
 public struct GradientText: View {
     let title: String
     let subtitle: String
+    var font: Font
+    var colors: [Color]
+    var shadowColor: Color
+    var shadowRadius: CGFloat
+    var shadowX: CGFloat
+    var shadowY: CGFloat
     
-    var font: Font = .system(size: FHKSize.size48, weight: .black, design: .rounded)
-    var colors: [Color] = [
-        Color(red: 0.9, green: 0.1, blue: 0.9), // Magenta
-        Color(red: 0.5, green: 0.3, blue: 0.9), // Violeta
-        Color(red: 0.3, green: 0.7, blue: 1.0), // Azul
-        Color(red: 0.4, green: 0.9, blue: 0.9)  // Cian
-    ]
-    
-    // Configuration shadow
-    var shadowColor: Color = .black.opacity(0.2)
-    var shadowRadius: CGFloat = 4
-    var shadowX: CGFloat = 2
-    var shadowY: CGFloat = 2
+    public init(title: String,
+                subtitle: String,
+                font: Font = .system(size: FHKSize.size48, weight: .black, design: .rounded),
+                colors: [Color] = [
+                    Color(red: 0.9, green: 0.1, blue: 0.9), // Magenta
+                    Color(red: 0.5, green: 0.3, blue: 0.9), // Violeta
+                    Color(red: 0.3, green: 0.7, blue: 1.0), // Azul
+                    Color(red: 0.4, green: 0.9, blue: 0.9)  // Cian
+                ],
+                shadowColor: Color = .black.opacity(0.2),
+                shadowRadius: CGFloat = FHKSize.size04,
+                shadowX: CGFloat = 2,
+                shadowY: CGFloat = 2
+    ) {
+        self.title = title
+        self.subtitle = subtitle
+        self.font = font
+        self.colors = colors
+        self.shadowColor = shadowColor
+        self.shadowRadius = shadowRadius
+        self.shadowX = shadowX
+        self.shadowY = shadowY
+    }
 
     public var body: some View {
         VStack(spacing: -10) {
