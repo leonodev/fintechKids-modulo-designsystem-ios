feed073c3401eda8ccf8a3848d5d364185973812
Fredy Leon <fredy.leon@ua.la.com>
2026-01-07 09:23:47 +0100

    add new view

---
+++ Sources/FHKDesignSystem/Components/Views/TextField/GradientBorderField.swift
@@ -0,0 +1,40 @@
+//
+//  GradientBorderField.swift
+//  FHKDesignSystem
+//
+//  Created by Fredy Leon on 7/1/26.
+//
+
+import SwiftUI
+
+public struct GradientBorderField: View {
+    @Binding var text: String
+    var placeholder: String
+    var isSecure: Bool = false
+    
+    let accentGradient = LinearGradient(
+        gradient: Gradient(colors: [Color.blue, Color.purple]),
+        startPoint: .topLeading,
+        endPoint: .bottomTrailing
+    )
+
+    public var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(accentGradient, lineWidth: 3)
+            Group {
+                if isSecure {
+                    SecureField(placeholder, text: $text)
+                } else {
+                    TextField(placeholder, text: $text)
+                }
+            }
+            .padding(.horizontal, 16)
+            .padding(.vertical, 12)
+            .background(Color.white)
+            .cornerRadius(10)
+            .padding(4)
+        }
+        .frame(height: 50)
+    }
+}
