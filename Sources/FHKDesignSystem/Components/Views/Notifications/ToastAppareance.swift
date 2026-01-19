68ea4d486d5db66c1f523ad927b54df11c70612b
Fredy Leon <fredy.leon@ua.la.com>
2026-01-19 16:33:00 +0100

    change name icons notifications

---
+++ Sources/FHKDesignSystem/Components/Views/Notifications/ToastAppareance.swift
@@ -0,0 +1,143 @@
+//
+//  ToastAppareance.swift
+//  FHKDesignSystem
+//
+//  Created by Fredy Leon on 19/1/26.
+//
+
+import Foundation
+import SwiftUI
+
+public enum ToastType {
+    case success
+    case error
+    case warning
+    case notification
+}
+
+public class ToastInfo {
+    public var type: ToastType
+    public var message: String
+    public var hasIcon: Bool
+    
+    public init(type: ToastType = .success, message: String = "", hasIcon: Bool = false) {
+        self.type = type
+        self.message = message
+        self.hasIcon = hasIcon
+    }
+}
+
+extension View {
+    func setToastStyle(isVisible: Binding<Bool>, info: ToastInfo) -> some View {
+        modifier(ToastModifier(isVisible: isVisible, info: info))
+    }
+}
+
+protocol ToastAppareanceProtocol {
+    var borderColor: Color { get }
+    var fontText: Font { get }
+    var cornerRadius: CGFloat { get }
+    var borderWidth: CGFloat { get }
+    var shadow: CGFloat { get }
+}
+
+
+extension ToastAppareanceProtocol {
+    var borderColor: Color {
+        return FHKColor.gray
+    }
+    
+    @MainActor
+    var fontText: Font {
+        return .PangramSans.medium(FHKSize.size20)
+    }
+    
+    var cornerRadius: CGFloat {
+        return CGFloat(FHKSize.size20)
+    }
+    
+    var borderWidth: CGFloat {
+        return CGFloat(1)
+    }
+    
+    var shadow: CGFloat {
+        return CGFloat(FHKSize.size04)
+    }
+}
+
+fileprivate struct ToastModifier: ViewModifier, ToastAppareanceProtocol {
+    @Binding var isVisible: Bool
+    let info: ToastInfo?
+    
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .top) {
+ 
+            if isVisible {
+                content
+                .font(fontText)
+                .foregroundColor(getTextColor())
+                .padding(.horizontal, FHKSize.size16)
+                .background(getBackgroundColor())
+                .cornerRadius(cornerRadius)
+                .shadow(radius: shadow)
+                .padding(.top, safeAreaPadding())
+                .transition(.move(edge: .top))
+                .offset(y: isVisible ? paddingOffset() : -UIScreen.main.bounds.height)
+                .animation(.easeInOut(duration: 2), value: isVisible)
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                        withAnimation {
+                            isVisible = false
+                        }
+                    }
+                }
+                .padding()
+                Spacer()
+            }
+        }
+    }
+    
+    private func getBackgroundColor() -> Color {
+        switch info?.type {
+        case .success:
+            return FHKColor.success
+        case .error:
+            return FHKColor.error
+        case .warning:
+            return FHKColor.warning
+        case .notification:
+            return FHKColor.basicBlack
+        default:
+            return Color.clear
+        }
+    }
+    
+    private func getTextColor() -> Color {
+        switch info?.type {
+        case .success, .error, .notification:
+            return FHKColor.basicWhite
+        case .warning:
+            return FHKColor.basicBlack
+        default:
+            return Color.clear
+        }
+    }
+    
+    private func paddingOffset() -> CGFloat {
+        let valuePadding: CGFloat = -100
+        print(valuePadding)
+        return valuePadding
+    }
+    
+    private func safeAreaPadding() -> CGFloat {
+        let keyWindow = UIApplication.shared.connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .flatMap { $0.windows }
+            .first { $0.isKeyWindow }
+        
+        let topPadding = keyWindow?.safeAreaInsets.top ?? 0
+        let finalTopPadding = topPadding >= 20 ? topPadding : 0
+        return finalTopPadding
+    }
+}
--- Sources/FHKDesignSystem/Components/Views/Notifications/ToastView.swift
+++ Sources/FHKDesignSystem/Components/Views/Notifications/ToastView.swift
@@ -5,197 +5,8 @@
 //  Created by Fredy Leon on 18/1/26.
 //
 
-import SwiftUI
-
 import Foundation
 import SwiftUI
-
-public enum ToastType {
-    case success
-    case error
-    case alert
-    case undefined
-}
-
-public class ToastInfo {
-    public var type: ToastType
-    public var message: String
-    public var hasIcon: Bool
-    
-    public init(type: ToastType = .success, message: String = "", hasIcon: Bool = false) {
-        self.type = type
-        self.message = message
-        self.hasIcon = hasIcon
-    }
-}
-
-extension View {
-    func toast(isVisible: Binding<Bool>, info: ToastInfo?) -> some View {
-        self.modifier(ToastModifier(isVisible: isVisible, info: info))
-    }
-    
-    func setToastStyle(isVisible: Binding<Bool>, info: ToastInfo) -> some View {
-        modifier(ToastModifier(isVisible: isVisible, info: info))
-    }
-    
-    func setToastIconStyle(type: ToastType) -> some View {
-        self.modifier(ToastIconModifier(type: type))
-    }
-}
-
-protocol ToastAppareanceProtocol {
-    var borderColor: Color { get }
-    var fontText: Font { get }
-    var cornerRadius: CGFloat { get }
-    var borderWidth: CGFloat { get }
-    var shadow: CGFloat { get }
-}
-
-
-extension ToastAppareanceProtocol {
-    var borderColor: Color {
-        return FHKColor.gray
-    }
-    
-    @MainActor
-    var fontText: Font {
-        return .PangramSans.medium(FHKSize.size20)
-    }
-    
-    var cornerRadius: CGFloat {
-        return CGFloat(20)
-    }
-    
-    var borderWidth: CGFloat {
-        return CGFloat(1)
-    }
-    
-    var shadow: CGFloat {
-        return CGFloat(5)
-    }
-}
-
-fileprivate struct ToastModifier: ViewModifier, ToastAppareanceProtocol {
-    @Binding var isVisible: Bool
-    let info: ToastInfo?
-    
-    
-    func body(content: Content) -> some View {
-        ZStack(alignment: .top) {
- 
-            if isVisible {
-                content
-                .font(fontText)
-                .foregroundColor(getTextColor())
-                .padding(.horizontal, 16)
-                .background(getBackgroundColor())
-                .cornerRadius(cornerRadius)
-                .shadow(radius: shadow)
-                .padding(.top, safeAreaPadding())
-                .transition(.move(edge: .top))
-                .offset(y: isVisible ? paddingOffset() : -UIScreen.main.bounds.height)
-                .animation(.easeInOut(duration: 2), value: isVisible)
-                .onAppear {
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                        withAnimation {
-                            isVisible = false
-                        }
-                    }
-                }
-                .padding()
-                Spacer()
-            }
-        }
-    }
-    
-    private func getBackgroundColor() -> Color {
-        switch info?.type {
-        case .success:
-            return FHKColor.success
-        case .error:
-            return FHKColor.error
-        case .alert:
-            return FHKColor.warning
-        case .undefined, .none:
-            return Color.clear
-        }
-    }
-    
-    private func getTextColor() -> Color {
-        switch info?.type {
-        case .success, .error:
-            return FHKColor.basicWhite
-        case .alert:
-            return FHKColor.basicBlack
-        case .undefined, .none:
-            return Color.clear
-        }
-    }
-    
-    private func paddingOffset() -> CGFloat {
-        let valuePadding: CGFloat = -100
-        print(valuePadding)
-        return valuePadding
-    }
-    
-    private func safeAreaPadding() -> CGFloat {
-        let keyWindow = UIApplication.shared.connectedScenes
-            .compactMap { $0 as? UIWindowScene }
-            .flatMap { $0.windows }
-            .first { $0.isKeyWindow }
-        
-        let topPadding = keyWindow?.safeAreaInsets.top ?? 0
-        let finalTopPadding = topPadding >= 20 ? topPadding : 0
-        return finalTopPadding
-    }
-    
-    private func hasNotch() -> Bool {
-        let keyWindow = UIApplication.shared.connectedScenes
-            .compactMap { $0 as? UIWindowScene }
-            .flatMap { $0.windows }
-            .first { $0.isKeyWindow }
-        
-        let topPadding = keyWindow?.safeAreaInsets.top ?? 0
-        return topPadding > 20
-    }
-    
-    private var iconSystemName: String {
-        switch info?.type {
-        case .success:
-            return "checkmark.circle.fill"
-        case .error:
-            return "xmark.circle.fill"
-        case .alert:
-            return "exclamationmark.circle.fill"
-        case .undefined, .none:
-            return ""
-        }
-    }
-}
-
-fileprivate struct ToastIconModifier: ViewModifier {
-    let type: ToastType
-    
-    func body(content: Content) -> some View {
-        content
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 30, height: 30)
-            .clipShape(Circle())
-            .foregroundColor(colorIcon)
-            .shadow(radius: 10)
-    }
-    
-    var colorIcon: Color {
-        switch type {
-        case .success, .error:
-            return FHKColor.basicWhite
-        case .alert:
-            return FHKColor.basicBlack
-        case .undefined:
-            return Color.clear
-        }
-    }
-}
 
 public struct ToastView: View {
     @Binding public var isVisible: Bool
@@ -208,7 +19,6 @@ public struct ToastView: View {
     }
     
     public var body: some View {
-        // Quitamos el if de aquí, dejamos que el MODIFIER controle la visibilidad
         HStack(spacing: 15) {
             if info.hasIcon {
                 Image(systemName: iconSystemName)
@@ -225,7 +35,7 @@ public struct ToastView: View {
             
             Spacer()
         }
-        .padding(.horizontal, FHKSize.size20)
+        .padding(.horizontal, FHKSize.size04)
         .padding(.vertical, FHKSize.size16)
         .frame(maxWidth: .infinity)
         .setToastStyle(isVisible: $isVisible, info: info)
@@ -237,10 +47,10 @@ public struct ToastView: View {
             return "checkmark.circle.fill"
         case .error:
             return "xmark.circle.fill"
-        case .alert:
+        case .warning:
             return "exclamationmark.circle.fill"
-        case .undefined:
-            return ""
+        case .notification:
+            return "wifi.exclamationmark.circle.fill"
         }
     }
 }
