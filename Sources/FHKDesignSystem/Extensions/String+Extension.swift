d7abf1df2562779cfd293bec64d7b308272b4d5d
Fredy Leon <fredy.leon@ua.la.com>
2026-01-24 09:51:43 +0100

    add extension

---
+++ Sources/FHKDesignSystem/Extensions/String+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  String+Extension.swift
+//  FHKDesignSystem
+//
+//  Created by Fredy Leon on 24/1/26.
+//
+
+import SwiftUI
+
+public extension String {
+    
+    public var getAvatar: Image {
+        Image(self, bundle: .module)
+    }
+}
