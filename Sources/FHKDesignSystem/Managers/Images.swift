f548628c2624a4ecc6d71b287ab68179c485991c
Fredy Leon <fredy.leon@ua.la.com>
2026-05-16 09:23:25 +0200

    new Images

--- Sources/FHKDesignSystem/Managers/Images.swift
+++ Sources/FHKDesignSystem/Managers/Images.swift
@@ -11,11 +11,24 @@ public extension Image {
     static let background = Image("bg", bundle: .module)
     static let backgroundDemo = Image("bg_demo", bundle: .module)
     
+    // Flags
     static let noneFlag = Image("worldwide-circle", bundle: .module)
     static let englandCircleFlag = Image("england-circle", bundle: .module)
     static let franceCircleFlag = Image("france-circle", bundle: .module)
     static let italyCircleFlag = Image("italy-circle", bundle: .module)
     static let spainCircleFlag = Image("spain-circle", bundle: .module)
+    
+    // Icons
     static let fintechkidsTime = Image("fintechkids_time", bundle: .module)
     static let fintechkidsCoins = Image("fintechkids_coins", bundle: .module)
+    
+    // Menus
+    static let menuLoansEnable = Image("loan_gold", bundle: .module)
+    static let menuLoansDisabled = Image("loan_gray", bundle: .module)
+    static let menuPaymentEnable = Image("payment_gold", bundle: .module)
+    static let menuPaymentDisabled = Image("payment_gray", bundle: .module)
+    static let menuSavingsEnable = Image("savings_gold", bundle: .module)
+    static let menuSavingsDisabled = Image("savings_gray", bundle: .module)
+    static let menuTransferEnable = Image("transfer_gold", bundle: .module)
+    static let menuTransferDisabled = Image("transfer_gray", bundle: .module)
 }
